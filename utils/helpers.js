5f9b5315a8194f409e72b9fc70c34a5a11504c63
ShivamS-design <shivamnadar05@gmail.com>
2025-04-12 20:39:24 -0500

    Add files via upload

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+export const filterObj = (obj, ...allowedFields) => {
+  const filteredObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      filteredObj[el] = obj[el];
+    }
+  });
+  return filteredObj;
+};
