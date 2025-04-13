de7ea9124d3bce7da630d2d804feaf8ffcc530ff
ShivamS-design <shivamnadar05@gmail.com>
2025-04-13 16:55:50 -0500

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -10,10 +10,6 @@ import AppError from '../utils/appError.js';
 
 // Configuration
 const MONGO_OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
   poolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
